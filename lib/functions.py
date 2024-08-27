1175b454a51ba8e841d1080e9ece34734af43858
SultanTipa1 <josephngumbaugush@gmail.com>
2024-08-27 15:40:04 +0300

    Adds all files

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,36 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Naureen!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default()
+greet_with_default("Sunny")
 
 def add(num1, num2):
-    pass
+    print(f" {num1} + {num2} = {num1 + num2}")
+
+add(1, 2)
 
 def halve(number):
-    pass
+    print(f"{number} /2 = {number / 2}")
+
+    return number/2
+
+halve(4)
+
+def add(num1, num2):
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+    return result
+
+add(45, 55)
